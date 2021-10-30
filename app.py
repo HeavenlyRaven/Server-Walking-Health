4cbcd273c2e7b52b8063ddfc3feca140f4308e33
Egor <36207275+HeavenlyRaven@users.noreply.github.com>
2021-10-31 02:44:19 +0300

    /medical/getDoctors API method implemented; AuthToken added

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, Response, request
 
 from utils.db_tools import get_connection as getcon
 
+AUTH_TOKEN = "25fg63278gf3bxzg6fs"
+
 app = Flask(__name__)
 
 
@@ -24,34 +26,40 @@ def login():
 def get_data():
     head = request.headers
     if "UserId" in head and "AuthToken" in head:
-        user_id = head["UserId"]
-        con = getcon()
-        cur = con.cursor()
-        cur.execute(f"SELECT login, fullname, isDoctor FROM users WHERE id=={user_id}")
-        user = cur.fetchone()
-        try:
-            user_data = dict(user)
-        except TypeError:
-            return {"code": 404, "message": "Queried user not found"}
-        else:
-            if user_data["isDoctor"]:
-                user_data.update({"patients": []})
-                cur.execute(f"SELECT login, fullname FROM users AS u INNER JOIN patients AS p ON p.id==u.id WHERE p.doctorId=={user_id}")
-                for patient in cur.fetchall():
-                    user_data["patients"].append(dict(patient))
+        if head["AuthToken"] == AUTH_TOKEN:
+            user_id = head["UserId"]
+            con = getcon()
+            cur = con.cursor()
+            cur.execute(f"SELECT login, fullname, isDoctor FROM users WHERE id=={user_id}")
+            user = cur.fetchone()
+            try:
+                user_data = dict(user)
+            except TypeError:
+                return {"code": 404, "message": "Queried user not found"}
             else:
-                cur.execute(f"SELECT login, fullname FROM users AS u INNER JOIN patients AS p ON u.id==p.doctorId WHERE p.id=={user_id}")
-                user_data.update({"doctor": dict(cur.fetchone())})
-            return {"code": 200, "message": "Queried user found successfully", "result": user_data}
-        finally:
-            con.close()
+                if user_data["isDoctor"]:
+                    cur.execute(f"SELECT login, fullname FROM users AS u INNER JOIN patients AS p ON p.id==u.id WHERE p.doctorId=={user_id}")
+                    user_data.update({"patients": list(map(dict, cur.fetchall()))})
+                else:
+                    cur.execute(f"SELECT login, fullname FROM users AS u INNER JOIN patients AS p ON u.id==p.doctorId WHERE p.id=={user_id}")
+                    user_data.update({"doctor": dict(cur.fetchone())})
+                return {"code": 200, "message": "Queried user found successfully", "result": user_data}
+            finally:
+                con.close()
+        else:
+            return {"code": 403, "message": "Wrong AuthToken"}
     else:
         return {"code": 400, "message": "Incorrect request"}
 
 
 @app.get('/medical/getDoctors')
 def get_doctors():
-    return Response()
+    con = getcon()
+    cur = con.cursor()
+    cur.execute("SELECT login, fullname FROM users WHERE isDoctor")
+    doctors = list(map(dict, cur.fetchall()))
+    con.close()
+    return {"code": 200, "message": "OK", "result": doctors}
 
 
 @app.get('/medical/getMessages')
