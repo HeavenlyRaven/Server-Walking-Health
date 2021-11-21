3e8a58fd5e7a8c745d5511cab2c2169df392f666
HeavenlyRaven <36207275+HeavenlyRaven@users.noreply.github.com>
2021-11-21 22:30:16 +0300

    Preflight request handler fixed and updated: new allowed headers added

--- utils/handlers.py
+++ utils/handlers.py
@@ -3,14 +3,14 @@ from flask import request, Response
 
 def preflight_request_handler(method):
     def wrapper():
+        response = Response()
+        response.access_control_allow_origin = '*'
         if request.method == 'OPTIONS':
-            response = Response()
-            response.access_control_allow_origin = '*'
             response.access_control_allow_methods = ['OPTIONS', 'GET', 'POST']
-            response.access_control_allow_headers = ['CurrentUserLogin', 'AuthToken']
+            response.access_control_allow_headers = ['CurrentUserLogin', 'AuthToken', 'Accept', 'Accept-Encoding', 'Content-Type', 'Origin']
             response.access_control_max_age = '86400'
-            return response
         else:
-            return method()
+            response.response = method()
+        return response
     wrapper.__name__ = method.__name__
     return wrapper
